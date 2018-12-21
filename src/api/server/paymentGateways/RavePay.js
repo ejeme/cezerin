3483b98d23ef06191bb924b101205508b4ef3c35
Ejeme <aramideeichie@yahoo.com>
2018-12-21 07:41:11 -0500

    updated rave payment

--- src/api/server/paymentGateways/RavePay.js
+++ src/api/server/paymentGateways/RavePay.js
@@ -1,4 +1,4 @@
-import Ravepay from 'ravepay';
+/*import stripePackage from 'stripe';
 import OrdersService from '../services/orders/orders';
 import OrdertTansactionsService from '../services/orders/orderTransactions';
 
@@ -16,12 +16,8 @@ const getPaymentFormSettings = options => {
 
 const processOrderPayment = async ({ order, gatewaySettings, settings }) => {
 	try {
-		const rave = Ravepay(
-			gatewaySettings.public_key,
-			gatewaySettings.secret_key,
-			PRODUCTION_FLAG
-		);
-		const charge = await rave.Card.charge({
+		const stripe = stripePackage(gatewaySettings.secret_key);
+		const charge = await stripe.charges.create({
 			amount: order.grand_total * 100,
 			currency: settings.currency_code,
 			description: `Order #${order.number}`,
@@ -30,26 +26,11 @@ const processOrderPayment = async ({ order, gatewaySettings, settings }) => {
 				order_id: order.id
 			},
 			source: order.payment_token
-		})
-			.then(resp => {})
-			.catch(err => {});
+		});
 
 		// status: succeeded, pending, failed
-		var payload = {
-			//From the Rave website
-			PBFPubKey: gatewaySettings.public_key,
-			transaction_reference: ref,
-			otp: ''
-		};
-		rave.Card.validate(payload)
-			.then(resp => {
-				//From the Rave Website
-				return resp.body;
-			})
-			.catch(err => {});
-
 		const paymentSucceeded =
-			charge.status === 'succeeded' || resp.body === true;
+			charge.status === 'succeeded' || charge.paid === true;
 
 		if (paymentSucceeded) {
 			await OrdersService.updateOrder(order.id, {
@@ -78,3 +59,4 @@ export default {
 	getPaymentFormSettings,
 	processOrderPayment
 };
+*/
